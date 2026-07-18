f6ef218dc707477ebdc4baaac3bcaca282735b4e
danielidoghe-beep <danielidoghe@gmail.com>
2026-07-18 15:31:58 +0100

    Update register.js

--- register.js
+++ register.js
@@ -1,6 +1,9 @@
 import {
     auth,
-    createUserWithEmailAndPassword
+    db,
+    createUserWithEmailAndPassword,
+    doc,
+    setDoc
 } from "./firebase.js";
 
 const form = document.getElementById("registerForm");
@@ -9,13 +12,28 @@ form.addEventListener("submit", async (e) => {
 
     e.preventDefault();
 
+    const firstName = document.getElementById("firstName").value.trim();
+    const lastName = document.getElementById("lastName").value.trim();
     const email = document.getElementById("email").value.trim();
-
     const password = document.getElementById("password").value;
 
     try {
 
-        await createUserWithEmailAndPassword(auth, email, password);
+        const userCredential =
+            await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+
+        await setDoc(
+            doc(db, "users", userCredential.user.uid),
+            {
+                firstName,
+                lastName,
+                email
+            }
+        );
 
         alert("Account created successfully!");
 
